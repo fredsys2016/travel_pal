2f603b68b055f881a11790d63550f0f810a021ea
fredsys <fredsys2016@gmail.com>
2019-05-08 05:30:09 +0100

    version1 Travel Pal

---
+++ platforms/browser/platform_www/cordova_plugins.js
@@ -0,0 +1,38 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-statusbar/www/statusbar.js",
+        "id": "cordova-plugin-statusbar.statusbar",
+        "pluginId": "cordova-plugin-statusbar",
+        "clobbers": [
+            "window.StatusBar"
+        ]
+    },
+    {
+        "file": "plugins/cordova-sqlite-legacy-build-support/www/SQLitePlugin.js",
+        "id": "cordova-sqlite-legacy-build-support.SQLitePlugin",
+        "pluginId": "cordova-sqlite-legacy-build-support",
+        "clobbers": [
+            "SQLitePlugin"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-screen-orientation/www/screenorientation.js",
+        "id": "cordova-plugin-screen-orientation.screenorientation",
+        "pluginId": "cordova-plugin-screen-orientation",
+        "clobbers": [
+            "cordova.plugins.screenorientation"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.2.2",
+    "cordova-plugin-console": "1.0.7",
+    "cordova-plugin-statusbar": "1.0.1",
+    "cordova-sqlite-legacy-build-support": "1.3.5",
+    "cordova-plugin-screen-orientation": "3.0.1"
+}
+// BOTTOM OF METADATA
+});
